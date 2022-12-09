af90a625985ee5e6480e25dc1f1e8447508fd913
nibor chhetri <rabinkrbaniya09@gmail.com>
2022-12-09 13:31:15 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:nibor_myshoppingapp/utils/routes.dart';
+import 'pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'pages/login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      //home: LoginPage(),
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/",
+      routes: {
+        "/": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
+      },
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  final int days = 11;
+  final String name = "nibor";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MyShoppingApp"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("welcome to $days  $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
