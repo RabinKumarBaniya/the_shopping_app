5279aa2d57a8214ae83fb0a387d2962a3db14d80
nibor <rabinkrbaniya09@gmail.com>
2022-12-11 21:38:04 +0545

    commit

---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final imageUrl =
+        "https://media-exp1.licdn.com/dms/image/C4E03AQGOZ4Vdu06OAg/profile-displayphoto-shrink_800_800/0/1650221948459?e=2147483647&v=beta&t=JDT7rx7zqtM9XR9OMKLuW6BAEMXNicnCIuAUybB6teo";
+    return Drawer(
+      child: Container(
+        color: Colors.grey,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                margin: EdgeInsets.zero,
+                accountName: Text("Rabin Kumar Baniya"),
+                accountEmail: Text("rabinkrbaniya09@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(imageUrl),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.settings,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Setting",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.mail,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Mail",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
