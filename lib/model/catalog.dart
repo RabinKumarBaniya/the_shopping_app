7a290f3499732094ed5eb99afa8f0def56ab0518
Rabin Kumar Baniya <100424729+RabinKumarBaniya@users.noreply.github.com>
2022-12-13 20:03:57 +0545

    Update catalog.dart

--- lib/model/catalog.dart
+++ lib/model/catalog.dart
@@ -1,5 +1,15 @@
 class CatalogModel {
-  static List<Item> items;
+  static List<Item> items = [
+    Item(
+        id: 1,
+        name: "Iphone 11",
+        detail: "64GB, Black - Unlocked ",
+        price: 999,
+        info:
+            "This product is eligible for a replacement or refund within 90-day of receipt if it does not work as expected.",
+        image:
+            "https://th.bing.com/th/id/OIP.FdW0-Cua_oFQK-CCPmiriwHaKJ?pid=ImgDet&rs=1")
+  ];
 }
 
 class Item {
@@ -11,30 +21,31 @@ class Item {
   final num price;
   final String image;
 
-  Item({required this.id,
-    required this.name,
-    required this.detail,
-    required this.info,
-    required this.price,
-    required this.image});
+  Item(
+      {required this.id,
+      required this.name,
+      required this.detail,
+      required this.info,
+      required this.price,
+      required this.image});
 
-factory Item.fromMap(Map<String, dynamic> map) {
-  return Item(
-    id: map["id"],
-    name: map["name"],
-    detail: map["detail"],
-    info: map["info"],
-    price: map["price"],
-    image: map["image"],
-  );
-}
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map["id"],
+      name: map["name"],
+      detail: map["detail"],
+      info: map["info"],
+      price: map["price"],
+      image: map["image"],
+    );
+  }
 
-toMap() => {
-      "id": id,
-      "name": name,
-      "detail": detail,
-      "info": info,
-      "price": price,
-      "image": image,
-    };
+  toMap() => {
+        "id": id,
+        "name": name,
+        "detail": detail,
+        "info": info,
+        "price": price,
+        "image": image,
+      };
 }
