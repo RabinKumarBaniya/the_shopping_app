f76584dab181614c2cbba952f5d9d555c6ad70cb
nibor <rabinkrbaniya09@gmail.com>
2022-12-13 19:38:42 +0545

    small changes in catelog

--- lib/model/catalog.dart
+++ lib/model/catalog.dart
@@ -5,18 +5,18 @@ class CatalogModel {
 class Item {
   final int id;
   final String name;
+
   final String detail;
   final String info;
   final num price;
   final String image;
 
-  Item(
-      {required this.id,
-      required this.name,
-      required this.detail,
-      required this.info,
-      required this.price,
-      required this.image});
+  Item({required this.id,
+    required this.name,
+    required this.detail,
+    required this.info,
+    required this.price,
+    required this.image});
 
 // factory Item.fromMap(Map<String, dynamic> map) {
 //   return Item(
