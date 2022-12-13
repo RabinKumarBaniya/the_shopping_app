0ad123d2053375d8e204db721d87eb57208058d8
nibor <rabinkrbaniya09@gmail.com>
2022-12-13 19:36:57 +0545

    catalog

---
+++ lib/model/catalog.dart
@@ -0,0 +1,40 @@
+class CatalogModel {
+  static List<Item> items;
+}
+
+class Item {
+  final int id;
+  final String name;
+  final String detail;
+  final String info;
+  final num price;
+  final String image;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.detail,
+      required this.info,
+      required this.price,
+      required this.image});
+
+// factory Item.fromMap(Map<String, dynamic> map) {
+//   return Item(
+//     id: map["id"],
+//     name: map["name"],
+//     detail: map["detail"],
+//     info: map["info"],
+//     price: map["price"],
+//     image: map["image"],
+//   );
+// }
+//
+// toMap() => {
+//       "id": id,
+//       "name": name,
+//       "detail": detail,
+//       "info": info,
+//       "price": price,
+//       "image": image,
+//     };
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nibor_myshoppingapp/model/catalog.dart';
 import 'package:nibor_myshoppingapp/widgets/drawer.dart';
+import 'package:nibor_myshoppingapp/widgets/item_widget.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 11;
+
   final String name = "nibor";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +43,15 @@ class HomePage extends StatelessWidget {
           "MyShoppingApp",
         ),
       ),
-      body: Center(
-        child: Container(
-          child: Text("welcome to $days  $name"),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: CatalogModel.items.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: CatalogModel.items[index],
+              );
+            }),
       ),
       drawer: MyDrawer(),
     );
