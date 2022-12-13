d71309c73d488bba2c8fcf16810d15dda9e5759e
Rabin Kumar Baniya <100424729+RabinKumarBaniya@users.noreply.github.com>
2022-12-13 19:49:44 +0545

    Update catalog.dart

--- lib/model/catalog.dart
+++ lib/model/catalog.dart
@@ -18,23 +18,23 @@ class Item {
     required this.price,
     required this.image});
 
-// factory Item.fromMap(Map<String, dynamic> map) {
-//   return Item(
-//     id: map["id"],
-//     name: map["name"],
-//     detail: map["detail"],
-//     info: map["info"],
-//     price: map["price"],
-//     image: map["image"],
-//   );
-// }
-//
-// toMap() => {
-//       "id": id,
-//       "name": name,
-//       "detail": detail,
-//       "info": info,
-//       "price": price,
-//       "image": image,
-//     };
+factory Item.fromMap(Map<String, dynamic> map) {
+  return Item(
+    id: map["id"],
+    name: map["name"],
+    detail: map["detail"],
+    info: map["info"],
+    price: map["price"],
+    image: map["image"],
+  );
+}
+
+toMap() => {
+      "id": id,
+      "name": name,
+      "detail": detail,
+      "info": info,
+      "price": price,
+      "image": image,
+    };
 }
